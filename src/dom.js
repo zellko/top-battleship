9dc54aea9b9d5745668e616a84dcce804ef48024
zellko <wyttenda@tutanota.com>
2022-07-14 15:59:22 +0200

    Added method to add instruction/result display

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,9 @@
+const display = document.querySelector('.display');
+const state = document.querySelector('.state');
+const message = document.querySelector('.message');
+const DomStartGame = document.querySelector('.btn-start');
+const DomRandomBtm = document.querySelector('.btn-random');
+
 const dom = (() => {
   const createBoard = (board) => {
     for (let index = 0; index < 100; index++) {
@@ -7,10 +13,87 @@ const dom = (() => {
     }
   };
   const clearBoard = (board) => {
-    console.log('clear board');
+    const boardCell = [...board.children];
+
+    for (let index = 0; index < boardCell.length; index++) {
+      const cell = boardCell[index];
+
+      cell.className = 'cell';
+    }
   };
 
-  const showShip = (board, cellNumber, shipLength, isValid, isVertical) => {
+  const displayShips = (gameBoard, domBoard) => {
+    // Display all ships on the board
+
+    // Get all ships position from Gameboard object
+    const shipsPosition = gameBoard.allShipsPosition;
+
+    // Color each cell with a ship in it
+    for (let index = 0; index < shipsPosition.length; index++) {
+      const posN = shipsPosition[index];
+      const cell = domBoard.children[posN];
+
+      cell.classList.add('ship');
+    }
+  };
+
+  const displayMiss = (gameBoard, domBoard) => {
+    // Display all that are missed on the board in blue color
+
+    // Get all missed position from Gameboard object
+    const missedPosition = gameBoard.missed;
+
+    // Color each cell which were "missed"
+    for (let index = 0; index < missedPosition.length; index++) {
+      const posN = missedPosition[index];
+      const cell = domBoard.children[posN];
+
+      cell.classList.add('miss');
+    }
+  };
+
+  const displayHit = (gameBoard, domBoard) => {
+    // Display cell that are "hit" on the board in red color
+
+    // Get all hit position from Gameboard object
+    const hitPosition = gameBoard.hit;
+
+    // Color each cell which were "hit"
+    for (let index = 0; index < hitPosition.length; index++) {
+      const posN = hitPosition[index];
+      const cell = domBoard.children[posN];
+
+      cell.classList.add('hit');
+    }
+  };
+
+  const displayMessage = (message1, message2, isWin) => {
+    display.classList.remove('hide');
+    state.textContent = message1;
+    message.textContent = message2;
+
+    if (isWin === 'init') {
+      display.classList.remove('win');
+      display.classList.remove('lost');
+      DomStartGame.textContent = 'Start Game';
+      DomRandomBtm.classList.remove('hide');
+    } else if (isWin === 'win') {
+      display.classList.add('win');
+      DomStartGame.textContent = 'Restart';
+      DomRandomBtm.classList.add('hide');
+    } else {
+      display.classList.add('lost');
+      DomStartGame.textContent = 'Restart';
+      DomRandomBtm.classList.add('hide');
+    }
+  };
+
+  const hideDisplay = () => {
+    display.className = 'display';
+    display.classList.add('hide');
+  };
+
+  const placeShip = (board, cellNumber, shipLength, isVertical, isValid) => {
     const firstCellRow = Math.floor(cellNumber / 10);
 
     for (let index = 0; index < shipLength; index++) {
@@ -20,43 +103,25 @@ const dom = (() => {
       if ((firstCellRow !== cellRow) && !isVertical) return; // avoid horizontal overflow
       if ((cellNumber + offset) > 99) return; // avoid vertical overflow
 
+      // board.children[cellNumber + offset].classList.add('moving');
+
       if (isValid) {
-        board.children[cellNumber + offset].classList.add('hl-valid');
+        board.children[cellNumber + offset].classList.add('moving');
       } else {
-        board.children[cellNumber + offset].classList.add('hl-invalid');
+        board.children[cellNumber + offset].classList.add('moving-invalid');
       }
     }
   };
 
-  const hideShip = (board) => {
-    const playerBoardArray = [...board.children];
-
-    for (let index = 0; index < playerBoardArray.length; index++) {
-      board.children[index].classList.remove('hl-valid');
-      board.children[index].classList.remove('hl-invalid');
-    }
-  };
-
-  const displayShips = (gameBoard, domBoard) => {
-    // Display all ships on the board
-    console.log(gameBoard);
-    console.log(domBoard.children);
-
-    // Get all ships position from Gameboard object
-    const shipsPosition = gameBoard.allShipsPosition;
-
-    // Color each cell with a ship in it
-    for (let index = 0; index < shipsPosition.length; index++) {
-      const posN = shipsPosition[index];
-      console.log(posN);
-      const cell = domBoard.children[posN];
-
-      cell.classList.add('ship');
-    }
-  };
-
   return {
-    createBoard, clearBoard, showShip, hideShip, displayShips,
+    createBoard,
+    clearBoard,
+    displayShips,
+    displayMiss,
+    displayHit,
+    displayMessage,
+    hideDisplay,
+    placeShip,
   };
 })();
 
